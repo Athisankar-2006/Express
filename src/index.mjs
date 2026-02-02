01c0fb1d03c91728cdf0cf78f07d58ab859b845b
Athisankar-2006 <athisankar2k6@gmail.com>
2026-02-02 23:03:09 +0530

    route and query params

--- src/index.mjs
+++ src/index.mjs
@@ -1,163 +1,117 @@
 import express from "express";
+// import productsRouter from "./routes/products.mjs"
+// import usersRouter from "./routes/users.mjs"
+// import { getParamsId,getUserIndexById } from "./utils/middlewares.mjs";
 
-const app =express()
+const app =express();
+
+// app.use(express.json())
+// app.use(usersRouter);
+// app.use(productsRouter)
+
 
 const port=3000;
 
 
-const users = [
-{id:1, user_name: "sankar"},
-{id:2, user_name: "athi"},
-{id:3, user_name: "spider"},
-{id:4, user_name: "kumar"},
-{id:5, user_name: "peter"},
-{id:6, user_name: "iron man"},
-{id:7, user_name: "enjoy"}
+
+
+
+
+
+const usersp=[
+{id:1, user_name:"Athi"},
+{id:2, user_name:"spider man"},
+{id:3, user_name:"bad man"},
+{id:4, user_name:"iron man"},
+{id:5, user_name:"super man"}
+
 ]
 
 
 
-const products = [
-{id:1, product_name: "poco"},
-{id:2, product_name: "infinix"},
-{id:3, product_name: "apple"},
-{id:4, product_name: "oppo"},
-{id:5, product_name: "vivo"},
-{id:6, product_name: "iq"},
-{id:7, product_name: "redmi"}
+
+const productsP=[
+{id:1, product_name:"apple"},
+{id:2, product_name:"linux"},
+{id:3, product_name:"kali"},
+{id:4, product_name:"window"},
+{id:5, product_name:"ios"}
+
 ]
+
+
+
+app.get("/api/users",(req,res)=>{
+
+    const {query:{filter,value}} =req;
+
+    if(filter&& value){
+        return res.send(usersp.filter(((user)=>user[filter].toLowerCase().includes(value))))
+    }
+    res.send(usersp);
+
+})
+
+
+app.get("/api/productsp",(req,res)=>{
+
+    const {query:{filter,value}} =req;
+
+    if(filter&& value){
+        return res.send(productsP.filter(((product)=>product[filter].toLowerCase().includes(value))))
+    }
+    res.send(productsP);
+
+})
+
+
+
+app.get("/api/productsp/:id",(req,res)=>{
+    // console.log(req.params.id)
+    const id=parseInt(req.params.id);
+   if(isNaN(id)){
+      return res.status(400).send({msg:"it is not a number"})
+   }
+
+   const user=usersp.find((user)=>user.id===id);
+   if(user){
+      return res.send(user)
+   }
+   return res.send({msg:"user not found"})
+})
+
+
+
+app.get("/api/productsP/:id",(req,res)=>{
+    // console.log(req.params.id)
+    const id=parseInt(req.params.id);
+   if(isNaN(id)){
+      return res.status(400).send({msg:"it is not a number"})
+   }
+
+   const product=productsP.find((user)=>product.id===id);
+   if(user){
+      return res.send(product)
+   }
+   return res.send({msg:"user not found"})
+})
+
+
+
+
+
 
 
 
 app.get("/",(req,res)=>{
+    // res.cookie("thisIsKey","thisIsValue",{maxAge: 60000})
     res.send({msg: "Root"})
 });
 
 
-app.get("/api/users",(req,res)=>{
-    const {query:{filter,value}}=req;
-    console.log(filter,value);
-    if(filter && value){
-      return res.send(users.filter((user)=>user[filter].toLowerCase().includes(value)))  
-    }
-   
-    res.send(users);
-})
-
-
-
-app.get("/api/products",(req,res)=>{
-    const {query:{filter,value}}=req;
-    console.log(filter,value);
-    if(filter && value){
-      return res.send(products.filter(((product)=>product[filter].toLowerCase().includes(value)))) 
-    }
-   
-    res.send(products);
-})
-
-app.get("/api/users",(req,res)=>{
-    res.send(users)
-})
-
-
-app.get("/api/users/:id",(req,res)=>{
-    const id= parseInt(req.params.id);
-    console.log(id);
-    if(isNaN(id)){
-       return res.status(400).send({msg:'bad request invaild id'})
-    }
-    const user=users.find((user)=>user.id === id);
-    if(user){
-       return res.send(user);
-    }
-    return res.status(400).send({msg:"user not found"})
-})
-
-
-
-
-app.get("/api/products/:id",(req,res)=>{
-    const id= parseInt(req.params.id);
-    console.log(id);
-    if(isNaN(id)){
-       return res.status(400).send({msg:'bad request invaild id'})
-    }
-    const product=products.find((product)=>product.id === id);
-    if(product){
-       return res.send(product);
-    }
-    return res.status(400).send({msg:"user not found"})
-})
-
-
-app.use(express.json())
-
-
-
-
-app.post("/api/users",(req, res)=>{
-    console.log(req.body);
-    const{body}=req;
-    const NewUser={id: users[users.length-1].id+1, ...body};
-    users.push(NewUser)
-    return res.status(201).send(NewUser);
-})
-
 
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`);
-})
-
-app.put("/api/users/:id",(req,res)=>{
-
-    const id= parseInt(req.params.id);
-    console.log(id);
-    if(isNaN(id)){
-       return res.status(400).send({msg:'bad request invaild id'})
-    }
-    const userIndex=users.find((user)=>user.id === id);
-    if(userIndex === -1){
-        return res.status(400).send({msg:"user not found"})
-    }
-    const {body}=req;
-    users[userIndex]={id:id, ...body}
-    return res.status(200).send({msg:"user updated"});
-    
-
-})
-
-
-app.patch("/api/users/:id",(req,res)=>{
-    const id= parseInt(req.params.id);
-    console.log(id);
-    if(isNaN(id)){
-       return res.status(400).send({msg:'bad request invaild id'})
-    }
-    const userIndex=users.find((user)=>user.id === id);
-    if(userIndex === -1){
-        return res.status(400).send({msg:"user not found"})
-    }
-    const {body} =req;
-    users[userIndex]={...users[userIndex], ...body};
-    res.sendStatus(200);
-
-})
-
-app.delete("/api/users/:id",(req,res)=>{
-   const id= parseInt(req.params.id);
-    console.log(id);
-    if(isNaN(id)){
-       return res.status(400).send({msg:'bad request invaild id'})
-    }
-    const userIndex=users.find((user)=>user.id === id);
-    if(userIndex === -1){
-        return res.status(400).send({msg:"user not found"})
-    }
-
-    users.splice(userIndex,1)
-    res.sendStatus(200)
-
 })
 
 
@@ -165,4 +119,3 @@ app.delete("/api/users/:id",(req,res)=>{
 
 //thunder client extention installled
 
-// put- update (complete update)
